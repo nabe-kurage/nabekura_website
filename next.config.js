58b657922e377105f02eebecdd37b9cd2b196886
nabe-kurage <tabiflag@gmail.com>
2020-08-13 20:42:03 +0900

    CSSファイルのURL確認

--- next.config.js
+++ next.config.js
@@ -5,5 +5,10 @@ module.exports = {
     }
     return config
   },
-  assetPrefix: '/nabekura_website'
+  //TODO 変更されているのは確認できたので、github pageでソース確認
+  // CSSファイル→/nabekura_website/_next/static
+  // /nabekura_website/_next/static/css/954dcc3934232109335e.css
+  // 画像ファイル→/nabekura_website/static
+
+  assetPrefix: '/nabekura_website/docs'
 };
