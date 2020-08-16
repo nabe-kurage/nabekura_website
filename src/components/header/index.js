8c525842590f76957f0c408d9f05ac52d3ffc63a
nabe-kurage <tabiflag@gmail.com>
2020-08-17 01:58:02 +0900

    headerファイル読み込み設定

---
+++ src/components/header/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Link from "next/link";
+
+import styles from "./index.module.scss";
+
+const Footer = () => {
+    return (
+        <header className={styles.header}>
+            {/* <Link href="/">
+                <a className={styles.logoWrapper}>nabekura</a>
+            </Link>
+            <nav className={styles.nav}>
+                <Link href="/about">
+                    <a className={styles.navItem}>about</a>
+                </Link>
+                <Link href="/work">
+                    <a className={styles.navItem}>work</a>
+                </Link>
+            </nav> */}
+        </header>
+    );
+};
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,15 @@ import React from "react";
 
 import styles from "./index.module.scss";
 
+import Header from "../components/header";
+
 // import Header from "../components/header";
 
 export default () => (
-  <div className={styles.mvWrappe}>
-    <img className={styles.mv} src="images/kv.jpg" alt="kv" />
-</div>
+  <>
+    <Header />
+    <div className={styles.mvWrappe}>
+      <img className={styles.mv} src="images/kv.jpg" alt="kv" />
+      </div>
+  </>
 );
