7194392a4f8478c50bf5eacefca1a2d1aacbcb1d
nabe-kurage <tabiflag@gmail.com>
2020-09-10 02:20:15 +0900

    workページ作成

---
+++ src/pages/work/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from "./index.module.scss";
+
+
+
+export default () => (
+	<>
+		<section className={styles.wrapper}>
+			<div className={styles.textWrapper}>
+				<p className={styles.authorName}>work</p>
+			</div>
+		</section>
+	</>
+);
