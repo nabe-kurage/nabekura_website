f8a5195bafa9d7dc10326e30869e02a6d062806d
nabe-kurage <tabiflag@gmail.com>
2020-09-19 02:29:32 +0900

    メタデータ追加

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,6 +14,9 @@ export default function MyApp({ Component, pageProps }) {
 				href="https://fonts.googleapis.com/css2?family=Righteous&family=Tomorrow:wght@500&display=swap"
 				rel="stylesheet"
 			/>
+			<meta name="viewport" content="width=device-width,initial-scale=1" />
+			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
 		</head>
 		<Header />
 		<main>
