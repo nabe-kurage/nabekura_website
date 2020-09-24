eacf8da48a254f6d4c66a2503c8e2ac8046176ba
nabe-kurage <tabiflag@gmail.com>
2020-09-25 01:06:18 +0900

    SP対応途中

--- src/components/mv/index.js
+++ src/components/mv/index.js
@@ -5,7 +5,7 @@ import styles from "./index.module.scss";
 const Mv = () => {
     return (
         <div className={styles.mvWrappe}>
-            <img className={styles.mv} src="/images/kv.jpg" alt="kv" />
+            <img className={styles.mv} src="/images/kv.jpg" alt="kv" />　
             {/* ~/src/, ~/ ~/images/ だめ */}
         </div>
     );
