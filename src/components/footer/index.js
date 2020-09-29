d54ab2d609c7919186b97dbd8426cbb12d5c3d9e
nabe-kurage <tabiflag@gmail.com>
2020-09-29 23:50:52 +0900

    small 修正

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,7 +6,7 @@ import styles from "./index.module.scss";
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <small>~&copy; 2020 nabekura~</small>
+            <small>&copy; 2020 nabekura</small>
         </footer>
     );
 };
